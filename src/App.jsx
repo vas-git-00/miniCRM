e43f0d39ea1044818b0290df3af42e6970d3d92b
vas-git-00 <vasyukov.a.o@gmail.com>
2024-12-20 14:40:21 +0300

    create table2

--- src/App.jsx
+++ src/App.jsx
@@ -23,25 +23,27 @@ function App() {
       key: "1",
       order: "24-1002",
       client: "Ромашка",
-      contact: "Иванов Иван",
+      //contact: "Иванов Иван",
       order_status: "В работе",
       pay_status: "Не оплачено",
       amount: "10 000,00",
-      amount_dolg:'5 000,00',
+      amount_dolg: "5 000,00",
       owner: "Глазунова Екатерина",
       order_date: "20.12.2024 13:40",
+      description:"Компания: ООО Ромашка, Контакт: Иванов Иван Иванович"
     },
     {
       key: "2",
       order: "24-1004",
       client: "ИП Васильев И.С.",
-      contact: "Васильев Иван",
+      //contact: "Васильев Иван",
       order_status: "В работе",
       pay_status: "Оплачено",
       amount: "12 000,00",
-      amount_dolg:'0,00',
+      amount_dolg: "0,00",
       owner: "Глазунова Екатерина",
       order_date: "20.12.2024 13:32",
+      description:"Hello"
     },
   ]
 
@@ -52,18 +54,30 @@ function App() {
       key: "order",
       render: (text) => <a>{text}</a>,
       width: 120, //ширина колонки
+      filters: [
+        {
+          text: "24-1002",
+          value: "24-1002",
+        },
+        {
+          text: "24-1004",
+          value: "24-1004",
+        },
+      ],
+      onFilter: (value, record) => record.order.startsWith(value),
+      filterSearch: true,
     },
     {
       title: "Клиент",
       dataIndex: "client",
       key: "client",
     },
-    {
-      title: "Контакт",
-      dataIndex: "contact",
-      key: "contact",
-      ellipsis: true, // сокращение если не влезают данные ...
-    },
+    //{
+    //  title: "Контакт",
+    //  dataIndex: "contact",
+    //  key: "contact",
+    //  ellipsis: true, // сокращение если не влезают данные ...
+    //},
     {
       title: "Статус заказа",
       dataIndex: "order_status",
@@ -98,6 +112,11 @@ function App() {
       key: "order_date",
     },
   ]
+
+  const onChange = (filters, sorter, extra) => {
+    console.log("params", filters, sorter, extra)
+  }
+
   //<Table dataSource={dataSource} columns={columns} />
   return (
     <>
@@ -150,10 +169,19 @@ function App() {
             }}
           >
             <Table
+              title={() => 'Заказы'}
               dataSource={dataSource}
               columns={columns}
               pagination={false}
               size="small"
+              onChange={onChange}
+              bordered={true}
+              expandable={{
+                expandedRowRender: (record) => (
+                  <p style={{ margin: 0 }}>{record.description}</p>
+                ),
+                rowExpandable: (record) => record.name !== "Not Expandable",
+              }}
             />
           </Content>
         </Layout>
